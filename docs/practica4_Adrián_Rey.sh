4b2c74be42063e408bb12064f02fe3e16f1abf31
Jhiyuko <adri.reylab@gmail.com>
2023-12-05 13:27:05 +0100

    paquetes incluídos

--- docs/practica4_Adrián_Rey.sh
+++ docs/practica4_Adrián_Rey.sh
@@ -7,7 +7,7 @@ if [ $# -eq 0 ]; then
 show_ayuda
 exit 1
 fi
-FICHERO='/home/$USER/Downloads/paquetes.txt'
+FICHERO='/home/$USER/Descargas/paquetes.txt'
 if [ ! -f $FICHERO ] || [ ! -r $FICHERO ]; then
 echo "El fichero $FICHERO no existe o no se puede leer"
 exit 2
